720e0e8880974725b1bba901af20629182a775ca
Olha Kirdiaieva <o.kirdiaieva@ukma.edu.ua>
2018-11-28 00:02:09 +0200

    Ajax is working! I must commit it until something goes wrong!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import './scss/main.scss';
 
-var u = 'http://nit.tron.net.ua/api/product/list';
+var categories;
+var products;
+
 $.ajax({
-    url: u
+    url: "http://nit.tron.net.ua/api/category/list"
 }).then(function (result) {
+    categories = result;
+    for(var i=0; i<categories.length; i++){
+        printCategory(categories[i].description)
+    }
 });
+
+$.ajax({
+    url: "http://nit.tron.net.ua/api/product/list"
+}).then(function (result) {
+    products = result;
+});
+
+var category;
+function printCategory(value) {
+    category = "<span class='category'>" + value + "</span>";
+    document.getElementById("categories-list").innerHTML += category;
+}
+
+var product;
+function createProduct(value){
+    product = "<div class='product-card'>" + value + "</div>";
+    document.getElementById("product-list").innerHTML += product;
+}
